6255b614ebb3d498149d86f0e389fd876aa7be2b
Wesley Tamiarana <wesley.tamiarana@gmail.com>
2025-04-17 01:14:32 +0200

    CNX: WEB Api -> Adicionado a configuracao inicial do projeto.
    Signed-off-by: Wesley Tamiarana <wesley.tamiarana@gmail.com>

---
+++ src/main/java/com/conexa/Aplicacao.java
@@ -0,0 +1,12 @@
+package com.conexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplicacao {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Aplicacao.class, args);
+	}
+}
